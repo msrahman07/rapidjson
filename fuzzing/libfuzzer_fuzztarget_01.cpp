e786352a7f24ee29bc6bf3f5e60989e823789ee0
msrahman <msrahman@csil-cpu2.cs.surrey.sfu.ca>
2020-04-13 12:44:07 -0700

    all fuzzing results

--- fuzzing/libfuzzer_fuzztarget_01.cpp
+++ fuzzing/libfuzzer_fuzztarget_01.cpp
@@ -1,11 +1,13 @@
+#include <iostream>
 #include <stdint.h>
 #include <stddef.h>
+#include "rapidjson/reader.h"
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
 using namespace rapidjson;
-
+using namespace std;
 /*
 bool FuzzMe(const uint8_t *Data, size_t DataSize) {
   return DataSize >= 3 &&
@@ -14,11 +16,45 @@ bool FuzzMe(const uint8_t *Data, size_t DataSize) {
       Data[2] == 'Z' &&
       Data[3] == 'Z';  // :‑<
 }*/
+struct MyHandler {
+    bool Null() { cout << "Null()" << endl; return true; }
+    bool Bool(bool b) { cout << "Bool(" << boolalpha << b << ")" << endl; return true; }
+    bool Int(int i) { cout << "Int(" << i << ")" << endl; return true; }
+    bool Uint(unsigned u) { cout << "Uint(" << u << ")" << endl; return true; }
+    bool Int64(int64_t i) { cout << "Int64(" << i << ")" << endl; return true; }
+    bool Uint64(uint64_t u) { cout << "Uint64(" << u << ")" << endl; return true; }
+    bool Double(double d) { cout << "Double(" << d << ")" << endl; return true; }
+    bool RawNumber(const char* str, SizeType length, bool copy) { 
+        cout << "Number(" << str << ", " << length << ", " << boolalpha << copy << ")" << endl;
+        return true;
+    }
+    bool String(const char* str, SizeType length, bool copy) { 
+        cout << "String(" << str << ", " << length << ", " << boolalpha << copy << ")" << endl;
+        return true;
+    }
+    bool StartObject() { cout << "StartObject()" << endl; return true; }
+    bool Key(const char* str, SizeType length, bool copy) {
+        cout << "Key(" << str << ", " << length << ", " << boolalpha << copy << ")" << endl;
+        return true;
+    }
+    bool EndObject(SizeType memberCount) { cout << "EndObject(" << memberCount << ")" << endl; return true; }
+    bool StartArray() { cout << "StartArray()" << endl; return true; }
+    bool EndArray(SizeType elementCount) { cout << "EndArray(" << elementCount << ")" << endl; return true; }
+};
 
-extern "C" int LLVMFuzzerTestOneInput(const char *Data, size_t Size) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   //FuzzMe(Data, Size);
-  Document d;
-  d.Parse(Data);
+  std::string input(reinterpret_cast<const char*>(Data), Size);
+  rapidjson::Document d;
+  d.Parse(input.data());
+  rapidjson::StringBuffer buffer;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  d.Accept(writer);
+
+  MyHandler handler;
+  Reader reader;
+  StringStream ss(input.data());
+  reader.Parse(ss, handler);
   return 0;
 }
 
